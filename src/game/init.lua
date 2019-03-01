1fed16aa1506ceb31f22ceea0c65eb5ba5909cae
Niels Nielsen <slein.nroh@gmail.com>
2019-03-01 22:27:39 +0100

    Add camera

--- src/game/init.lua
+++ src/game/init.lua
@@ -1,14 +1,19 @@
 local path  = "src/game/"
 
-local level = require(path .. "level")
-local game  = {}
+local level  = require(path .. "level")
+local camera = require(path .. "camera")
+
+local game   = {}
 
 function game:load()
     level:load("res/levels/0.png")
+
+    self.camera = camera(0, 0, 2, 2, 0)
 end
 
 function game:update(dt)
-
+    self.camera:set()
+    self.camera:unset()
 end
 
 function game:draw()
