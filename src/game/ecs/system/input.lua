bc2277191d835865b884ffaff39da6c5eb5dc657
Asger <ab@carbuddii.com>
2019-03-02 15:21:28 +0100

    input

--- src/game/ecs/system/input.lua
+++ src/game/ecs/system/input.lua
@@ -3,6 +3,7 @@ s.input.update = function(i, input, direction)
     input.left = love.keyboard.isDown("left")
     input.right = love.keyboard.isDown("right")
     input.up = not input.up and (love.keyboard.isDown("up") or love.keyboard.isDown("z"))
+    input.respawn = love.keyboard.isDown("backspace")
     direction[1] = (input.left and 1 or input.right and -1 or direction[1])
 end
 
